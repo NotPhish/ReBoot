a9651c43ec587a253785bdf12a8056604c92e8bc
NotPhish <203453798+NotPhish@users.noreply.github.com>
2026-04-25 23:40:42 +0200

    UPDATE: You can now select a profile picture from your local device via pressing on the profileContainer in Settings

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,7 @@
 import { View } from "react-native";
 import { useState } from "react";
+import * as ImagePicker from "expo-image-picker";
+
 import { globalStyles } from "@/styles/global";
 import StatusContainer from "@/container/statusContainer";
 import Counter from "@/container/counter";
@@ -9,6 +11,19 @@ import Counter from "@/container/counter";
 
 export default function Index() {
   const [timerAcitve, setTimerActive] = useState(false);
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert("Please set a Profile Picture as .png");
+    }
+  };
 
   return (
     <View style={globalStyles.container}>
--- container/profileContainer.tsx
+++ container/profileContainer.tsx
@@ -1,24 +1,33 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
 import { Image } from "expo-image";
+
 import { globalStyles } from "@/styles/global";
 
 // ToDo: Make it pull the pfp uploaded by the user from
 // (for now) local storage (NO cloud **yet**)
+//
+type Props = {
+  profilePicturePath: string;
+  onPress?: () => void;
+};
 
 const PlaceholderProfilePicture = require("@/assets/images/dummy-pfp.png");
 
-export default function ProfileContainer() {
+export default function ProfileContainer({
+  onPress,
+  profilePicturePath,
+}: Props) {
   return (
-    <View style={style.container}>
+    <Pressable style={style.container} onPress={onPress}>
       <View>
         <Image
-          source={PlaceholderProfilePicture}
+          source={profilePicturePath}
           style={style.profilePictureContainer}
         ></Image>
       </View>
       <Text style={globalStyles.headerText}>*Username*</Text>
       <Text style={globalStyles.text}>*Settings description*</Text>
-    </View>
+    </Pressable>
   );
 }
 
